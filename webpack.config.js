3d271a114e4db0c2e063cbf74fa06af952968225
Mohamed.rashed <mohamed.rashed@cyshield.com>
2024-12-18 13:15:57 +0200

    Update webpack config to get all js in dist folder and sub folders and update readme file

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,59 @@
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
 // Get all files in the dist directory
-const distPath = path.resolve(__dirname, "dist/angular19-babel/browser");
-const entryFiles = fs.readdirSync(distPath).reduce((entries, file) => {
-  const filePath = path.join(distPath, file);
-  if (fs.statSync(filePath).isFile() && path.extname(file) === ".js") {
-    entries[file.replace(".js", "")] = filePath;
-  }
+const distPath = path.resolve(__dirname, 'dist/angular19-babel/browser');
+
+function getJsFiles(directory) {
+  const entries = {};
+
+  fs.readdirSync(directory).forEach(file => {
+    const filePath = path.join(directory, file);
+    const fileStats = fs.statSync(filePath);
+
+    if (fileStats.isDirectory()) {
+      // Recursively process subdirectories
+      Object.assign(entries, getJsFiles(filePath));
+    } else if (fileStats.isFile() && path.extname(file) === '.js') {
+      // Add JS files to entries
+      const relativePath = path
+        .relative(distPath, filePath)
+        .replace(/\\/g, '/');
+      entries[relativePath.replace('.js', '')] = filePath;
+    }
+  });
+
   return entries;
-}, {});
+}
+
+const entryFiles = getJsFiles(distPath);
 
 // Path to the generated index.html
-const indexPath = path.join(__dirname, 'dist', 'angular19-babel/browser', 'index.html');
+const indexPath = path.join(
+  __dirname,
+  'dist',
+  'angular19-babel/browser',
+  'index.html'
+);
 
 // Read the index.html file
 let indexContent = fs.readFileSync(indexPath, 'utf8');
 
 // Add type="text/javascript" to all <script> tags
-indexContent = indexContent.replace(/<script /g, '<script type="text/javascript" ');
+indexContent = indexContent.replace(
+  /<script /g,
+  '<script type="text/javascript" '
+);
 
 // Write the modified content back to the file
 fs.writeFileSync(indexPath, indexContent, 'utf8');
 
 module.exports = {
-  target: ["browserslist"],
+  target: ['browserslist'],
   entry: entryFiles,
   output: {
     path: distPath,
-    filename: "[name].js",
+    filename: '[name].js',
   },
   module: {
     rules: [
@@ -36,17 +61,15 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: [
-                    "@babel/preset-env"
-            ],
+            presets: ['@babel/preset-env'],
           },
         },
       },
     ],
   },
-  mode: "production", // or 'development' based on your needs
+  mode: 'production', // or 'development' based on your needs
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
